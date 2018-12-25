d842d27f9886afad4f47f5990bb076676aa6b899
Andrei Metelski <8metya8@gmail.com>
2018-12-25 19:15:01 +0300

    Add test logick for Lesson_2_Task_3

--- Lesson_2_Task_3_Login/Lesson_2_Task_3_Login/UnitTest1.cs
+++ Lesson_2_Task_3_Login/Lesson_2_Task_3_Login/UnitTest1.cs
@@ -1,28 +1,43 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
 
 namespace Lesson_2_Task_3_Login
 {
     [TestFixture]
     
-    public class TestLogin
+    public class Lesson_2_Task_3
     {
-        IWebDriver driver;
+        private IWebDriver driver;
+        private WebDriverWait wait;
 
         [SetUp]
         public void Start()
         {
             driver = new ChromeDriver();
+
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
             driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(15);
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+
+            driver.Manage().Window.Maximize();
         }
 
         [Test]
-        public void TestMethod1()
+        public void TestLogin()
         {
+            driver.Navigate().GoToUrl("http://localhost/litecart/admin/login.php");
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
 
+            wait.Until(ExpectedConditions.UrlMatches("http://localhost/litecart/admin/"));
+            driver.FindElement(By.XPath("//ul[@id='box-apps-menu']/li[1]")).Click();
+            wait.Until(ExpectedConditions.TitleContains("Template"));
+            driver.FindElement(By.XPath("//a[@title='Logout']")).Click();
+            wait.Until(ExpectedConditions.UrlMatches("http://localhost/litecart/admin/login.php"));
         }
 
         [TearDown]
