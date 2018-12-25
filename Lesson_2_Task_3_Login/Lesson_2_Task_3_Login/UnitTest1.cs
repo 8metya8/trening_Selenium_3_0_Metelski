a4720e4e9bf1c9da15f80a670652069df1a83e8e
Andrei Metelski <8metya8@gmail.com>
2018-12-25 18:47:25 +0300

    Create Lesson_2_Task_3

---
+++ Lesson_2_Task_3_Login/Lesson_2_Task_3_Login/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace Lesson_2_Task_3_Login
+{
+    [TestFixture]
+    
+    public class TestLogin
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Start()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(15);
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
