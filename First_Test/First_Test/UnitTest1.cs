c505b884a5b31d860a656c1722a432620328ee8e
Andrei Metelski <8metya8@gmail.com>
2018-12-25 11:06:42 +0000

    Update First Task
    Добавлены все необходимые дополнения (NUnit, NUnit Tets Adapter, Selenium WebDriver, Selenium Support)

--- First_Test/First_Test/UnitTest1.cs
+++ First_Test/First_Test/UnitTest1.cs
@@ -1,14 +1,35 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
-namespace First_Test
+namespace Task_1
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        private IWebDriver driver;
+        
+        [SetUp]
+        public void Start()
         {
+            driver = new ChromeDriver();           
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(15);
+            driver.Manage().Window.Maximize();
         }
+
+        [Test]
+        public void FirstTest()
+        {
+            driver.Navigate().GoToUrl("https://tut.by/");
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+
     }
 }
