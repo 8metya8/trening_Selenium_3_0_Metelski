d712a1e17cc114df850a03e97ca2bf18c906f9ca
Andrei Metelski <8metya8@gmail.com>
2019-01-11 00:57:30 +0300

    Task_7
    Start

---
+++ Tasks/Task_7/Helpers/Helpers.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_7
+{
+    public class HelperLogin
+    {
+        public static void LogInAdmin(IWebDriver driver)
+        {
+            driver.Navigate().GoToUrl("http://localhost/litecart/admin");
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+        }
+    }
+}
---
+++ Tasks/Task_7/Task_7.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.Generic;
+
+namespace Task_7
+{
+
+    [TestFixture]
+
+    public class Task_7
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [Test]
+        public void TestTask7()
+        {
+            IList<IWebElement> menu = new List<IWebElement>();
+            IList<IWebElement> subMenu = new List<IWebElement>();
+            IList<IWebElement> elements;
+            string text;
+
+            HelperLogin.LogInAdmin(driver);
+
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            wait.Until(d => d.FindElement(By.CssSelector("li.selected")));
+            Assert.IsTrue(driver.FindElements(By.CssSelector("td#content>h1")).Count == 1);
+/*
+            menu = driver.FindElements(By.CssSelector("ul#box-apps-menu>li"));
+
+            foreach (IWebElement itemOfMenu in menu)
+            {
+                itemOfMenu.Click();
+                wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+                wait.Until(d => d.FindElement(By.CssSelector("li.selected")));
+
+                Assert.IsTrue(driver.FindElements(By.CssSelector("td#content>h1")).Count == 1);
+
+                menu = driver.FindElements(By.CssSelector("ul#box-apps-menu>li"));
+                
+                                subMenu = driver.FindElements(By.CssSelector("li.selected li"));
+
+                                if(subMenu != null)
+                                {
+                                    foreach(IWebElement itemOfSubMenu in subMenu)
+                                    {
+                                        itemOfMenu.Click();
+
+                                        wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+                                        wait.Until(d => d.FindElement(By.CssSelector("li.selected")));
+
+                                        Assert.IsTrue(driver.FindElements(By.CssSelector("td#content>h1")).Count == 1);
+                                    }
+                                }
+            }*/
+            
+        }
+
+        [SetUp]
+        public void Start()
+        {
+            driver = new ChromeDriver();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(15);
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+
+            driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+
+   
+}
