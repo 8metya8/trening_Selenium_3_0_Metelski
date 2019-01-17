5cd5fa7dce95b0d2cf5ae78937e062107a0397fb
Andrei Metelski <8metya8@gmail.com>
2019-01-18 00:14:19 +0300

    Update Task_10.cs

--- Tasks/Lesson_5/Task_10.cs
+++ Tasks/Lesson_5/Task_10.cs
@@ -1,30 +1,40 @@
 ﻿using System;
 using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System.Collections.Generic;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
 
 namespace Lesson_5
 {
-    [TestFixture]
 
-    class Task_10
+     [TestFixture(typeof(ChromeDriver))]
+     [TestFixture(typeof(FirefoxDriver))]
+     [TestFixture(typeof(EdgeDriver))]
+
+    class Task_10<TWebDriver> where TWebDriver : IWebDriver, new()
     {
-        private IWebDriver driver;
+        private TWebDriver driver;
         private WebDriverWait wait;
 
         [Test]
+
+    //    [TestCase(typeof(ChromeDriver))]
+     //   [TestCase(typeof(FirefoxDriver))]
+     //   [TestCase(typeof(EdgeDriver))]
+
         public void TestTask_10()
         {
-
             driver.Navigate().GoToUrl("http://localhost/litecart/en/rubber-ducks-c-1/");
 
             IList<IWebElement> allDucks = driver.FindElements(By.CssSelector(".product"));
             string link;
             IWebElement element;
 
-            for(int i = 0; i < allDucks.Count; i++)
+            for (int i = 0; i < allDucks.Count; i++)
             {
                 element = allDucks[i].FindElement(By.CssSelector(".link"));
                 link = element.GetAttribute("href");
@@ -37,12 +47,13 @@ namespace Lesson_5
                 driver.Navigate().GoToUrl("http://localhost/litecart/en/rubber-ducks-c-1/");
                 allDucks = driver.FindElements(By.CssSelector(".product"));
             }
+
         }
 
         [SetUp]
         public void Start()
         {
-            driver = new ChromeDriver();
+            driver = new TWebDriver();
 
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
             driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(15);
@@ -55,7 +66,7 @@ namespace Lesson_5
         public void Stop()
         {
             driver.Quit();
-            driver = null;
+           // driver = null;
         }
     }
 }
