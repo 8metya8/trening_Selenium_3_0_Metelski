5d433dc85d8bbfb30dcdcb06df7588ca27dc5f46
Andrei Metelski <8metya8@gmail.com>
2019-01-17 23:22:13 +0300

    Create TestTast_10

--- Tasks/Lesson_5/Task_10.cs
+++ Tasks/Lesson_5/Task_10.cs
@@ -7,19 +7,36 @@ using System.Collections.Generic;
 
 namespace Lesson_5
 {
+    [TestFixture]
+
     class Task_10
     {
         private IWebDriver driver;
         private WebDriverWait wait;
 
         [Test]
-        public void TestTask9_2()
+        public void TestTask_10()
         {
 
-            HelperLogin.LogInAdmin(driver);
-            driver.Navigate().GoToUrl("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones");
+            driver.Navigate().GoToUrl("http://localhost/litecart/en/rubber-ducks-c-1/");
 
+            IList<IWebElement> allDucks = driver.FindElements(By.CssSelector(".product"));
+            string link;
+            IWebElement element;
 
+            for(int i = 0; i < allDucks.Count; i++)
+            {
+                element = allDucks[i].FindElement(By.CssSelector(".link"));
+                link = element.GetAttribute("href");
+                element.Click();
+
+                wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("[name=add_cart_product]")));
+
+                Assert.IsTrue(driver.Url.Equals(link));
+
+                driver.Navigate().GoToUrl("http://localhost/litecart/en/rubber-ducks-c-1/");
+                allDucks = driver.FindElements(By.CssSelector(".product"));
+            }
         }
 
         [SetUp]
@@ -29,7 +46,7 @@ namespace Lesson_5
 
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
             driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(15);
-            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
 
             driver.Manage().Window.Maximize();
         }
@@ -42,4 +59,3 @@ namespace Lesson_5
         }
     }
 }
-
