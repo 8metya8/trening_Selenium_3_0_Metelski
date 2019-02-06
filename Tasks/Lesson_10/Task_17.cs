cfde121aef7b24bcb9001230153e0fc2a7833520
Andrei Metelski <8metya8@gmail.com>
2019-02-06 20:39:31 +0300

    Added Task_17

---
+++ Tasks/Lesson_10/Task_17.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using OpenQA.Selenium;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.Generic;
+
+namespace Lesson_10
+{
+    [TestFixture]
+    public class Task_17
+    {
+        IWebDriver driver;
+        WebDriverWait wait;
+
+        [SetUp]
+        public void Start()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.SetLoggingPreference(LogType.Browser, LogLevel.All);
+                     
+            driver = new ChromeDriver(options);
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(25);
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void TestTask_17()
+        {
+            IList<IWebElement> listOfEditLinks;
+
+            HelperLogin.LogInAdmin(driver);
+
+            driver.Navigate().GoToUrl("http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1");
+            listOfEditLinks = driver.FindElements(By.CssSelector("[title=Edit]"));
+
+            for(int i = 2; i < listOfEditLinks.Count; i++)
+            {
+                listOfEditLinks[i].Click();
+                wait.Until(ExpectedConditions.ElementIsVisible(By.Name("save")));
+
+                //Check that console hasn't message of Severe Type (Error and otjer crirical messages)
+                foreach (LogEntry l in driver.Manage().Logs.GetLog("browser"))
+                {
+                    Assert.False(l.Level.Equals(LogLevel.Severe));
+                }
+
+                //Go back to catalog
+                driver.Navigate().GoToUrl("http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1");
+                //update list after reload page
+                listOfEditLinks = driver.FindElements(By.CssSelector("[title=Edit]"));
+            }
+            
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
---
+++ Test_remote/Test_remote/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using NUnit.Framework;
+using OpenQA.Selenium.IE;
+
+namespace Test_remote
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        IWebDriver driver;
+
+        [Test]
+        public void TestMethod1()
+        {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.SetCapability(CapabilityType.BrowserName, "firefox");
+
+            driver = new RemoteWebDriver(new Uri("http://192.168.56.1:4444/wd/hub"), capabilities);
+            driver.Manage().Window.Maximize();
+
+            driver.Navigate().GoToUrl("http://barancev.github.io/how-to-set-datepicker-value/");
+            driver.Quit();
+        }
+    }
+}
